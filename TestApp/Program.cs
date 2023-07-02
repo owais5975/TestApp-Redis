cf2eaa866d2a39b9605c5e05e9c533aae22bab66
owais5975 <79499299+owais5975@users.noreply.github.com>
2023-07-02 16:45:03 +0500

    configure swagger

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -18,11 +18,10 @@ builder.Services.AddStackExchangeRedisCache(opt =>
 });
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseHttpsRedirection();
