afd81d9b8d54d40a2584271ef5e9b8096ee09c6c
owais5975 <79499299+owais5975@users.noreply.github.com>
2023-07-06 00:48:24 +0500

    Web API log(Watch dogs)

--- TestApp/Helpers/CacheHelper.cs
+++ TestApp/Helpers/CacheHelper.cs
@@ -4,13 +4,11 @@ namespace TestApp.Helpers
 {
     public class CacheHelper
     {
-        public static DistributedCacheEntryOptions CacheOptions()
-        {
-            var distributedCacheEntryOptions = new DistributedCacheEntryOptions();
-            distributedCacheEntryOptions.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5*60);
-            distributedCacheEntryOptions.SlidingExpiration = null;
-
-            return distributedCacheEntryOptions;
-        }
+        public static DistributedCacheEntryOptions CacheOptions() =>
+            new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5 * 60),
+                SlidingExpiration = null
+            };
     }
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using TestApp.Entities;
 using TestApp.Services;
+using WatchDog;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,17 +15,22 @@ builder.Services.AddStackExchangeRedisCache(opt =>
     opt.Configuration = builder.Configuration.GetConnectionString("redis");
     opt.InstanceName = "Staging-free-db";
 });
+builder.Services.AddWatchDogServices(opt =>
+{
+    opt.IsAutoClear = true; // clears the logs after a specific duration
+    opt.ClearTimeSchedule = WatchDog.src.Enums.WatchDogAutoClearScheduleEnum.Monthly; // specify when it will automatically clear the logs
+});
 
 var app = builder.Build();
-
 app.UseSwagger();
 app.UseSwaggerUI();
-
-
-app.UseHttpsRedirection();
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseWatchDogExceptionLogger();
+app.UseWatchDog(opt =>
+{
+    opt.WatchPageUsername = "admin";
+    opt.WatchPagePassword = "Pass123#";
+});
 app.Run();
